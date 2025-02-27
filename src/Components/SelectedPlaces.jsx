6e18829919ec56249ca2312848255a1663f4bfff
Ummam Ali (Lappy) <sblue7446@gmail.com>
2025-02-27 17:05:56 +0500

    added titles and subtitles to selected section

--- src/Components/SelectedPlaces.jsx
+++ src/Components/SelectedPlaces.jsx
@@ -5,7 +5,13 @@ import { placesData } from "../store/places";
 export default function SelectedPlaces({ selections, deselectPlace }) {
   return (
     <div className="max-w-7xl mx-auto border border-gray-300 rounded-sm px-8 py-8 mb-4">
-      <h1 className="text-center text-xl mb-4">Your Bucketlist</h1>
+      <div className="text-center mb-10">
+        <h1 className="text-xl">Destination Wishlist</h1>
+        <p className="text-black/70 text-sm  max-w-sm mx-auto">
+          Keep track of all the amazing places you want to visit and turn your
+          travel dreams into reality.
+        </p>
+      </div>
       {selections.length === 0 && (
         <p className="text-center text-black/75 text-sm">
           No places to display! Please choose a place from the available
